6fe21ea68499953c798b89338fcd72fea86b213b
EpshitaNinawe <epshitaninawe@gmail.com>
2025-02-10 21:19:31 +0530

    update for details

--- hiringManagementTool/urls.py
+++ hiringManagementTool/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ClientManagerMasterViewSet, ClientMasterViewSet, InternalDepartmentMasterViewSet, LOBMasterViewSet, LocationMasterViewSet, OpenDemandViewSet, EmployeeMasterViewSet
+from .views import (
+    ClientManagerMasterViewSet,
+    ClientMasterViewSet,
+    InternalDepartmentMasterViewSet,
+    LOBMasterViewSet,
+    LocationMasterViewSet,
+    OpenDemandViewSet,
+    EmployeeMasterViewSet,
+    ClientDetailsViewSet,
+    LocationDetailsViewSet,
+    DemandStatusDetailsViewSet,
+    InternalDepartmentDetailsViewSet,
+    LOBDetailsViewSet,
+)
 
 router = DefaultRouter()
 router.register(r'open-demands', OpenDemandViewSet)
@@ -10,6 +23,11 @@ router.register(r'location-master', LocationMasterViewSet)
 router.register(r'employee-master', EmployeeMasterViewSet)
 router.register(r'internal-department-master', InternalDepartmentMasterViewSet)
 router.register(r'client-manager-master', ClientManagerMasterViewSet)
+router.register(r'clients-details', ClientDetailsViewSet, basename="client-details")
+router.register(r'location-details', LocationDetailsViewSet, basename="location-details")
+router.register(r'demand-status-details', DemandStatusDetailsViewSet, basename="demand-status-details")
+router.register(r'internal-department-details', InternalDepartmentDetailsViewSet, basename='internal-department-details')
+router.register(r'lob-details', LOBDetailsViewSet, basename="lob-details")
 
 urlpatterns = [
     path('', include(router.urls)),
--- hiringManagementTool/views.py
+++ hiringManagementTool/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
-from .models import ClientMaster, InternalDepartmentMaster, LOBMaster, LocationMaster, OpenDemand, EmployeeMaster, ClientManagerMaster
-from .serializers import ClientMasterSerializer, ClientManagerMasterSerializer, InternalDepartmentMasterSerializer, LOBMasterSerializer, LocationMasterSerializer, OpenDemandSerializer, EmployeeMasterSerializer
+from .models import ClientMaster, InternalDepartmentMaster, LOBMaster, LocationMaster, OpenDemand, EmployeeMaster, ClientManagerMaster, DemandStatusMaster
+from .serializers import ClientMasterSerializer, ClientManagerMasterSerializer, InternalDepartmentMasterSerializer, LOBMasterSerializer, LocationMasterSerializer, OpenDemandSerializer, EmployeeMasterSerializer, LocationDetailsSerializer, DemandStatusDetailsSerializer, InternalDepartmentDetailsSerializer, LOBDetailSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import ClientMaster
+from .serializers import ClientDetailsSerializer
 
 class OpenDemandViewSet(viewsets.ModelViewSet):
     queryset = OpenDemand.objects.all()
@@ -10,6 +14,16 @@ class OpenDemandViewSet(viewsets.ModelViewSet):
 class ClientMasterViewSet(viewsets.ModelViewSet):
     queryset = ClientMaster.objects.all()
     serializer_class = ClientMasterSerializer
+
+class ClientDetailsViewSet(viewsets.ReadOnlyModelViewSet):  # ReadOnlyModelViewSet allows only GET requests
+    queryset = ClientMaster.objects.all()
+    serializer_class = ClientDetailsSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Override list to return only required fields"""
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 # Client Manager Master API View
 class ClientManagerMasterViewSet(viewsets.ModelViewSet):
@@ -35,3 +49,50 @@ class InternalDepartmentMasterViewSet(viewsets.ModelViewSet):
 class EmployeeMasterViewSet(viewsets.ModelViewSet):
     queryset = EmployeeMaster.objects.all()
     serializer_class = EmployeeMasterSerializer
+
+#client details
+class ClientDetailsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ClientMaster.objects.all()
+    serializer_class = ClientDetailsSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Override list to return only required fields"""
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+
+class LocationDetailsViewSet(viewsets.ReadOnlyModelViewSet):  # ReadOnlyModelViewSet allows only GET requests
+    queryset = LocationMaster.objects.all()
+    serializer_class = LocationDetailsSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Override list to return only required fields"""
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+class DemandStatusDetailsViewSet(viewsets.ReadOnlyModelViewSet):  # ReadOnlyModelViewSet allows only GET requests
+    serializer_class = DemandStatusDetailsSerializer
+
+    def get_queryset(self):
+        """Sort records by dsm_sortid in ascending order"""
+        return DemandStatusMaster.objects.all().order_by('dsm_sortid')
+
+    def list(self, request, *args, **kwargs):
+        """Override list to return only required fields"""
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    
+class InternalDepartmentDetailsViewSet(viewsets.ReadOnlyModelViewSet):  # ReadOnly API
+    queryset = InternalDepartmentMaster.objects.select_related(
+        'idm_deliverymanager_id__emp_lcm_id'  # Optimized query for related fields
+    ).order_by('idm_id')
+    serializer_class = InternalDepartmentDetailsSerializer
+
+class LOBDetailsViewSet(viewsets.ReadOnlyModelViewSet):
+    """API View to fetch LOB details with client partner & delivery manager."""
+    queryset = LOBMaster.objects.all()
+    serializer_class = LOBDetailSerializer
