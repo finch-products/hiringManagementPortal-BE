a233e445454f10818bfd0e485afaaa1c0355e0b2
EpshitaNinawe <epshitaninawe@gmail.com>
2025-02-27 20:02:51 +0530

    api issue resolved

--- hiringManagementTool/components/demands/urls.py
+++ hiringManagementTool/components/demands/urls.py
@@ -4,7 +4,7 @@ from hiringManagementTool.components.demands.views import DemandDetailAPIView, D
 urlpatterns = [
     path("", DemandAPIView.as_view(), name="all-demands"),
     # path("/all", DemandsAPIView.as_view(), name="all-demands"),
-    path("<str:id>/", DemandDetailAPIView.as_view(), name="demand-details"), 
+    path("id/<str:id>/", DemandDetailAPIView.as_view(), name="demand-details"), 
     path('update-demand-status/', OpenDemandUpdateAPIView.as_view(), name='update-demand-status'),
     path('filter/', FilterDemandsAPIView.as_view(), name='filter-demands'),
 ]
