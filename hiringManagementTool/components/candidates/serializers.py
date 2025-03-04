4cbba215ecd72fb6a13a515708b88e580abce97b
EpshitaNinawe <epshitaninawe@gmail.com>
2025-03-04 20:26:39 +0530

    candidate api endpoint updated

--- hiringManagementTool/components/candidates/serializers.py
+++ hiringManagementTool/components/candidates/serializers.py
@@ -1,7 +1,37 @@
 from hiringManagementTool.models.candidates import CandidateMaster
 from rest_framework import serializers
+from hiringManagementTool.models.candidatestatus import CandidateStatusMaster
+
+
+class CandidateStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CandidateStatusMaster
+        fields = ["csm_id", "csm_code"]
+
 
 class CandidateMasterSerializer(serializers.ModelSerializer):
+    candidate_status = CandidateStatusSerializer(source='cdm_csm_id', read_only=True)
+
+    cdm_insertby = serializers.CharField(source='cdm_insertby.emp_id', read_only=True)
+    cdm_updateby = serializers.CharField(source='cdm_updateby.emp_id', read_only=True)
+
     class Meta:
         model = CandidateMaster
-        fields = '__all__'
+        fields = [
+            "cdm_id",
+            "cdm_emp_id",
+            "cdm_name",
+            "cdm_email",
+            "cdm_phone",
+            "cdm_profile",
+            "cdm_description",
+            "cdm_keywords",
+            "cdm_insertdate",
+            "cdm_updatedate",
+            "cdm_isinternal",
+            "cdm_isactive",
+            "cdm_location",
+            "candidate_status",
+            "cdm_insertby",
+            "cdm_updateby"
+        ]
