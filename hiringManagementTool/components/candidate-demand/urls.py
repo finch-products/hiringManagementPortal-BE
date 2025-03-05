6ca36a4ca2fd5bdce48ca4df392aaaac35be7106
Vanshika Panjwani <vanshikapanjwani0105@gmail.com>
2025-03-05 22:22:27 +0530

    get candidate by demand service error resolved

--- hiringManagementTool/components/candidate-demand/urls.py
+++ hiringManagementTool/components/candidate-demand/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from .views import GetCandidatelistByDemandId
-
-urlpatterns = [
-    path('candidateby_opendemand/', GetCandidatelistByDemandId.as_view(), name='candidate-by-open-demand-api'),
-]
-from django.urls import path
 from .views import CandidateDemandLinkAPIView
 
 urlpatterns = [
+    path('candidateby_opendemand/', GetCandidatelistByDemandId.as_view(), name='candidate-by-open-demand-api'),
     path("", CandidateDemandLinkAPIView.as_view(), name="all-candiate-demand-links"),
 ]
+
+
  
