8631700e77cebf255eda73627bc4c141d3c1fd4c
EpshitaNinawe <epshitaninawe@gmail.com>
2025-03-11 17:09:52 +0530

    candidates not linked to a particular demand service

--- hiringManagementTool/components/candidatedemand/urls.py
+++ hiringManagementTool/components/candidatedemand/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import GetCandidatelistByDemandId
-from .views import CandidateDemandLinkAPIView
+from .views import CandidateDemandLinkAPIView, GetNonCandidatelistByDemandId
 
 urlpatterns = [
     path('candidateby_opendemand/', GetCandidatelistByDemandId.as_view(), name='candidate-by-open-demand-api'),
     path("", CandidateDemandLinkAPIView.as_view(), name="all-candiate-demand-links"),
+    path('not_added_candidatebydemandid/', GetNonCandidatelistByDemandId.as_view(), name='non-added-candidate-by-open-demand-api'),
 ]
 
 
