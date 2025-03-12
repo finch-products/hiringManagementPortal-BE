69a485e1c48b12abd6146b16c54f8b5a2b0addc2
Vanshika Panjwani <vanshikapanjwani0105@gmail.com>
2025-03-12 13:00:19 +0530

    backend model fixes

--- hiringManagementTool/models/demands.py
+++ hiringManagementTool/models/demands.py
@@ -15,7 +15,7 @@ class OpenDemand(models.Model):
         editable=False, 
         help_text="Formatted Primary Key (Auto-generated for Demand Entry) dem_ddmmyyyy_01"
     )
-    dem_ctoolnumber = models.CharField(max_length=50, help_text="Client Tool Number from Client")
+    dem_ctoolnumber = models.CharField(max_length=50, blank=True, null=True,help_text="Client Tool Number from Client")
     dem_ctooldate = models.DateTimeField(blank=True, null=True,help_text="Requirement/Demand Date from Client")
     dem_position_name = models.TextField(blank=True, null=True, help_text="Name of the position")
     # dem_cmm_id = models.ForeignKey(
@@ -49,8 +49,8 @@ class OpenDemand(models.Model):
     dem_lob_id = models.ForeignKey(
         'LOBMaster',
         on_delete=models.SET_NULL,
-         null=True,  # Allows NULL values in the database
-         blank=True,
+        null=True, # Allows NULL values in the database
+         #blank=True,
         related_name="lob_demands",
         db_column="dem_lob_id",
         help_text="Reference to LOB Master Table"
@@ -75,7 +75,9 @@ class OpenDemand(models.Model):
         help_text="Reference to Internal Department Master Table"
     )
 
-    dem_positions = models.IntegerField(blank=True, null=True,help_text="Number of Positions Required")
+    dem_positions = models.IntegerField(#blank=True, null=True,
+                                        default=0,
+                                        help_text="Number of Positions Required")
     dem_rrnumber = models.CharField(blank=True, null=True,max_length=50, help_text="Request Number")
     dem_jrnumber = models.TextField(blank=True, null=True, help_text="JR Number if External")
     dem_rrgade = models.CharField(max_length=100, blank=True, null=True, help_text="RR Grade Entry")
