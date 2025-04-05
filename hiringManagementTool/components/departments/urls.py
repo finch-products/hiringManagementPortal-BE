1f81a2f227f87995abc1ff93a5defcd97841af92
EpshitaNinawe <epshitaninawe@gmail.com>
2025-04-05 21:12:52 +0530

    department api for details of department with the employee details

--- hiringManagementTool/components/departments/urls.py
+++ hiringManagementTool/components/departments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from hiringManagementTool.components.departments.views import InternalDepartmentAPIView
+from hiringManagementTool.components.departments.views import InternalDepartmentAPIView, InternalDepartmentMasterListAPI
 
 urlpatterns = [
-    path("", InternalDepartmentAPIView().as_view(), name="dept-details")
+    path("", InternalDepartmentAPIView().as_view(), name="dept-details"),
+    path('details/', InternalDepartmentMasterListAPI.as_view(), name='internal-department-with-emp-details'),
 ]
