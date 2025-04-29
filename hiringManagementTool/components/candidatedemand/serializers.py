31ff20b4840286bebc1b084f125da0d2d1a33a89
Shrawani Karemore <139235828+shrawani287@users.noreply.github.com>
2025-04-29 14:08:47 +0530

    Update serializers.py

--- hiringManagementTool/components/candidatedemand/serializers.py
+++ hiringManagementTool/components/candidatedemand/serializers.py
@@ -161,11 +161,11 @@ class CandidateMasterSerializer(serializers.ModelSerializer):
 
 class OpenDemandResponseSerializer(serializers.Serializer):
     cdl_dem_id = serializers.CharField()
-    candidate_count = serializers.SerializerMethodField()
+    application_count = serializers.SerializerMethodField()
     demand_details = DemandSerializer()
     candidates = CandidateSerializer(many=True)
 
-    def get_candidate_count(self, obj):
+    def get_application_count(self, obj):
         return len(obj['candidates']) 
 
 class NonlinkedResponseSerializer(serializers.Serializer):
